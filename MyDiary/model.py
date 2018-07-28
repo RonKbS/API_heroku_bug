855f2367e90fcb19a2ecb1581c8db4cf1d45a188
RonKbS <stephen.bamwite@gmail.com>
2018-07-28 21:55:14 +0300

    Remove the init fn in Diary class

--- MyDiary/model.py
+++ MyDiary/model.py
@@ -5,15 +5,11 @@ from datetime import datetime
 which will enable more distinguishing between
 several users'''
 
-entries = []
-entry_id = 1
 
 
 class Diary:
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
+    entry_id = 1
+    entries = []
 
     @staticmethod
     def add_entry(enter_content):
@@ -24,22 +20,22 @@ class Diary:
         new_entry = {
             "date": date.strftime('%A.%B.%Y'),
             "content": content,
-            "ID": entry_id
+            "ID": Diary.entry_id
         }
-        for entry in entries:
-            if entries == []:
-                entries.append(new_entry)
+        for entry in Diary.entries:
+            if Diary.entries == []:
+                Diary.entries.append(new_entry)
                 return entry
             elif new_entry["content"] == entry["content"]:
                 return "New entry is similar to older entry"
             else:
                 entry["ID"] = entry["ID"] + 1
-        entries.append(new_entry)
+        Diary.entries.append(new_entry)
         return new_entry
 
     @staticmethod
     def find_entry_by_id(entry_id):
-        for entry in entries:
+        for entry in Diary.entries:
             if entry_id == entry["ID"]:
                 return entry
         return 'No such entry'
@@ -48,7 +44,7 @@ class Diary:
     
     @staticmethod
     def modify_entry(entry_id, content):
-        for entry in entries:
+        for entry in Diary.entries:
             if entry_id == entry["ID"]:
                 entry["content"] = content
                 return entry
@@ -56,14 +52,14 @@ class Diary:
 
     @staticmethod
     def delete_entry(entry_id):
-        for entry in entries:
+        for entry in Diary.entries:
             if entry_id == entry['ID']:
-                entries.remove(entry)
+                Diary.entries.remove(entry)
                 return "Entry deleted"
         return 'No such entry'
 
     @staticmethod
     def list_all_entries():
-        if entries != []:
-            return entries
+        if Diary.entries != []:
+            return Diary.entries
         return 'No entries'
